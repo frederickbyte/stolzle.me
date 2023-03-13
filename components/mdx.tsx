3c4bb095eea1c538dd40a57b019dc135734fb2aa
Frederick Stolzle <stolzle@hey.com>
2023-03-13 14:52:50 -0500

    Big Tech verbiage updates

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -2,22 +2,26 @@ import * as React from 'react';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import { Post } from '@/.contentlayer/generated';
 
-// Custom component for rendering callouts in posts.
-const Callout = (props: any) => {
+const CalloutWithLabel = (props: any) => {
   return (
-    <div className="flex bg-neutral-100 border border-neutral-200 rounded p-4 text-stone-800">
-      <div className="w-full callout">{props.children}</div>
+    <div>
+      <div className="bg-stone-700 px-2 text-white w-fit p-1 rounded relative left-7 font-bold top-4  ">
+        {props.title}
+      </div>
+      <div className={'border-2 border-stone-700 text-stone-800 p-3 rounded'}>
+        {props.children}
+      </div>
     </div>
   );
 }
 
-const CalloutWithLabel = (props: any) => {
+const InfoCalloutWithLabel = (props: any) => {
   return (
     <div>
-      <div className="bg-stone-700 px-2 text-white w-fit p-1 rounded-lg font-sans relative left-7 font-bold top-4  ">
+      <div className="bg-stone-700 px-2 text-white w-fit p-1 rounded relative left-5 font-bold top-4  ">
         {props.title}
       </div>
-      <div className={'border-2 border-stone-700 text-stone-800 p-3 rounded bg-blue-100'}>
+      <div className={'text-[0.95em] border-2 border-stone-700 text-stone-800 p-3 rounded bg-blue-200'}>
         {props.children}
       </div>
     </div>
@@ -25,8 +29,8 @@ const CalloutWithLabel = (props: any) => {
 }
 
 const customComponents = {
-  Callout,
-  CalloutWithLabel
+  CalloutWithLabel,
+  InfoCalloutWithLabel
 };
 
 interface MdxProps {
@@ -37,7 +41,7 @@ export function Mdx({ post }: MdxProps) {
   const Component = useMDXComponent(post.body.code);
 
   return (
-    <article className='prose prose-stone lg:prose-base mb-8'>
+    <article className='prose prose-sm prose-stone md:prose-base mb-8'>
       <Component components={{ ...customComponents }} />
     </article>
   );
